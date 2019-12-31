66009fdf5c580b50a0c91fd93bc5a3f014ab8cc2
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2019-12-31 14:45:15 -0800

    Merged Object-Oriented File Changes into main sketch file

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
+// Cell class
+class Cell {
+	constructor(x, y, cellType) {
+		this.x = x;
+		this.y = y;
+		this.cellType = cellType;
+	}
+}
+
 // Sketch constants
 const canvasSize = 600;
-const gridSize = 50;
+const gridSize = 10;
 const gridScale = canvasSize / gridSize;
 const obstacles = (gridSize * gridSize) / 4;
 
@@ -13,12 +22,12 @@ let pathFound = false;
 
 // Cell types to colors mapping
 let cellColors = {
-	'E': '#ffffff',
-	'O': '#000000',
-	'S': '#ff1919',
-	'G': '#6eff2b',
-	'V': '#ababab',
-	'P': '#6bf0ff',
+	'empty': '#ffffff',
+	'obstacle': '#000000',
+	'start': '#ff1919',
+	'goal': '#6eff2b',
+	'visited': '#ababab',
+	'path': '#6bf0ff',
 };
 
 // Initialize grid
@@ -29,7 +38,7 @@ function defineGrid() {
 	for(row = 0; row < gridSize; row++) {
 		let gridRow = [];
 		for(col = 0; col < gridSize; col++) {
-			gridRow.push('E');
+			gridRow.push(new Cell(col, row, 'empty'));
 		}
 		grid.push(gridRow);
 	}
@@ -37,7 +46,7 @@ function defineGrid() {
 
 function setRandomObstacles(numObstacles) {
 	for(i = 0; i < numObstacles; i++) {
-		grid[~~random(gridSize - 1)][~~random(gridSize - 1)] = 'O';
+		grid[~~random(gridSize - 1)][~~random(gridSize - 1)].cellType = 'obstacle';
 	}
 }
 
@@ -45,18 +54,24 @@ function search(cell) {
 	let x = cell.x;
 	let y = cell.y;
 
-	let neighbors = [createVector(x, y + 1), createVector(x, y - 1),
-					 createVector(x + 1, y), createVector(x - 1, y)];
+	let neighbors = [grid[y][x + 1], grid[y][x - 1]];
 
-	neighbors.forEach(function(v) {
-		if(v.x >= 0 && v.x <= gridSize - 1 && v.y >= 0 && v.y <= gridSize - 1) {
-			if(v.x == goal.x && v.y == goal.y) {
+	if(y >= 1) {
+		neighbors[2] = grid[y - 1][x];
+	}
+	if(y <= gridSize - 2) {
+		neighbors[3] = grid[y + 1][x];
+	}
+
+	neighbors.forEach(function(c) {
+		if(c != undefined) {
+			if(c.x == goal.x && c.y == goal.y) {
 				goalFound = true;
-				cameFrom[v.x + ',' + v.y] = x + ',' + y;
-			} else if(grid[v.y][v.x] == 'E') {
-				grid[v.y][v.x] = 'V';
-				cameFrom[v.x + ',' + v.y] = x + ',' + y;
-				frontier.push(v);
+				cameFrom[c.x + ',' + c.y] = x + ',' + y;
+			} else if(grid[c.y][c.x].cellType == 'empty') {
+				grid[c.y][c.x].cellType = 'visited';
+				cameFrom[c.x + ',' + c.y] = x + ',' + y;
+				frontier.push(c);
 			}
 		}
 	})
@@ -64,11 +79,15 @@ function search(cell) {
 
 function retrace() {
 	let current = goal.x + ',' + goal.y;
-	while(current != (start.x + ',' + start.y)) {
+	while(true) {
 		next = cameFrom[current];
 		x = parseInt(next.slice(0, next.search(',')), 10);
 		y = parseInt(next.slice(next.search(',') + 1, next.length), 10);
-        grid[y][x] = 'P';
+		if(grid[y][x].cellType != 'start') {
+			grid[y][x].cellType = 'path';
+		} else {
+			break;
+		}
         current = next;
 	}
 	pathFound = true;
@@ -81,12 +100,12 @@ function setup() {
 	setRandomObstacles(obstacles);
 
 	// Define start and goal
-	start = createVector(~~random(gridSize - 1), ~~random(gridSize - 1));
-	grid[start.y][start.x] = 'S';
+	start = grid[~~random(gridSize - 1)][~~random(gridSize - 1)];
+	start.cellType = 'start';
 	cameFrom[start.x + ',' + start.y] = '';
 
-	goal = createVector(~~random(gridSize - 1), ~~random(gridSize - 1));
-	grid[goal.y][goal.x] = 'G';
+	goal = grid[~~random(gridSize - 1)][~~random(gridSize - 1)];
+	goal.cellType = 'goal';
 
 	frontier.push(start);
 }
@@ -101,7 +120,7 @@ function draw() {
 
 	for(y = 0; y < gridSize; y++) {
 		for(x = 0; x < gridSize; x++) {
-			fill(cellColors[grid[y][x]]);
+			fill(cellColors[grid[y][x].cellType]);
 			rect(x * gridScale, y * gridScale, gridScale, gridScale);
 		}
 	}
