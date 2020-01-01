b1230cfb5b45c84f77191601ecfa07f27f2e4ebd
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2019-12-31 19:50:02 -0800

    Moved code for cell class into separate file. For real this time :)

---
+++ cell.js
@@ -0,0 +1,39 @@
+// Cell class (add new file for this)
+class Cell {
+	constructor(x, y, cellType, color) {
+		this.x = x;
+		this.y = y;
+		this.cellType = cellType;
+		this.color = color;
+	}
+
+	setAsStart() {
+		this.cellType = 'start';
+		this.color = colorScheme['start'];
+		cameFrom[this.x + ',' + this.y] = '';
+		frontier.push(this);
+	}
+
+	setAsGoal() {
+		this.cellType = 'goal';
+		this.color = colorScheme['goal'];
+	}
+
+	makeObstacle() {
+		this.cellType = 'obstacle';
+		this.color = colorScheme['dark-cyan'];
+	}
+
+	visit(originCell) {
+		this.cellType = 'visited';
+		this.color = colorScheme['light-green'];
+		cameFrom[this.x + ',' + this.y] = originCell.x + ',' + originCell.y;
+		frontier.push(this);
+	}
+
+	addToPath() {
+		this.cellType = 'path';
+		this.color = colorScheme['cyan'];
+	}
+
+}
